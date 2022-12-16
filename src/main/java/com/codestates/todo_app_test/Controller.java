8128b88e7d62e3cf287426e33843f8a9bf391dd2
jackcmh1 <jackcmh1@gmail.com>
2022-12-16 15:04:41 +0900

    To-do Application! 출력

--- src/main/java/com/codestates/todo_app_test/Controller.java
+++ src/main/java/com/codestates/todo_app_test/Controller.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Controller {
+
     @GetMapping
     public String helloWorld() {
         return "To-do Application !";
